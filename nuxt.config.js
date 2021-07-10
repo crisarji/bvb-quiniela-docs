b982acb9d3789209572e5cadeff370e3ec020e82
Cristian Arce Jimenez <cristian.arce@Cristian-Arce-MacBook-Pro.local>
2021-07-10 06:26:37 -0600

    circleci config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,14 @@
+const routerBase =
+  process.env.DEPLOY_ENV === "GH_PAGES"
+    ? {
+        router: {
+          base: "/bvb-quiniela-docs/"
+        }
+      }
+    : {};
+
 export default {
+  ...routerBase,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'bvb-quiniela-docs',
