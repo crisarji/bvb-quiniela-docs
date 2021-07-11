41b20d7962870952082246c8991f001a68f0e59a
Cristian Arce Jimenez <cristian.arce@Cristian-Arce-MacBook-Pro.local>
2021-07-10 23:55:11 -0600

    test deploy to circle with multi language ap

---
+++ components/Article.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-editable="blok" class="prose my-4 mx-auto p-2">
+    <div>
+      <h1>{{ blok.name }}</h1>
+      <p>{{ blok.intro }}</p>
+      <img class="w-64 h-128 float-none" :src="blok.image1.filename" alt="" />
+      <rich-text-renderer :document="blok.body" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
---
+++ components/Teaser.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-editable="blok" class="pb-8 mb-6 font-bold text-center">
+    <h2 class="text-6xl font-normal leading-normal mt-0 mb-2 text-blueGray-800">
+      {{ blok.headline }}
+    </h2>
+    <p class="text-xl font-semibold leading-relaxed mt-6 mb-4 text-blueGray-800">
+      {{ blok.paragraph }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
