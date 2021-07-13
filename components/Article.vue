fee3a4047022c0eaab6ecfebeb2389645e9b9c54
Cristian Arce Jimenez <cristian.arce@Cristian-Arce-MacBook-Pro.local>
2021-07-13 08:21:01 -0600

    New favicon and nuxt config

--- components/Article.vue
+++ components/Article.vue
@@ -2,7 +2,6 @@
   <div v-editable="blok" class="prose my-4 mx-auto p-2">
     <div>
       <h1>{{ blok.name }}</h1>
-      <rich-text-renderer :document="blok.intro" />
       <rich-text-renderer :document="blok.body" />
     </div>
   </div>
