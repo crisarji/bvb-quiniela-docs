a8e1433d7c33bc000650a3afa8534f47ce4d9f02
Cristian Arce Jimenez <cristian.arce@Cristian-Arce-MacBook-Pro.local>
2021-07-14 16:40:00 -0600

    test scopes

--- components/Header.vue
+++ components/Header.vue
@@ -6,7 +6,7 @@
           <span class="font-bold text-gray-800 text-xl text-left">BVB-Quiniela</span>
         </nuxt-link>
       </div>
-      <div class="flex items-center flex-shrink-0 text-black">
+      <!-- <div class="flex items-center flex-shrink-0 text-black">
         <nuxt-link class="text-black hover:underline" to="/">
           English
         </nuxt-link>
@@ -14,7 +14,7 @@
         <nuxt-link class="text-black hover:underline" to="/es">
           Español
         </nuxt-link>
-      </div>
+      </div> -->
     </nav>
   </header>
 </template>
