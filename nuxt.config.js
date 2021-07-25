6d6bf354d2a8688223ad4a2cfa19eea89393c33a
Cristian Arce Jimenez <cristian.arce@Cristian-Arce-MacBook-Pro.local>
2021-07-25 13:48:31 -0600

    Changes for icon and statis target and new deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,13 @@ export default {
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }]
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "bvb-quiniela-docs/favicon.png"
+      }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
